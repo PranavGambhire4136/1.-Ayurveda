844b38925b22fa717dcbfa6c83a892a6a36042c0
PranavGambhire4136 <160487419+PranavGambhire4136@users.noreply.github.com>
2025-01-12 18:19:22 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -28,7 +28,7 @@ app.use(cookieParser());
 
 // CORS configuration
 app.use(cors({
-    origin: "https://root-of-ayurveda.netlify.app",
+    origin: "http://localhost:4173",
     credentials: true, // Allows cookies to be sent
 }));
 
