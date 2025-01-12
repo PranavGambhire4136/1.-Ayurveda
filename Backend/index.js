c5d665b79ac5ee879af27c10290d305d914ca18d
PranavGambhire4136 <160487419+PranavGambhire4136@users.noreply.github.com>
2025-01-12 12:13:43 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -28,7 +28,7 @@ app.use(cookieParser());
 
 // CORS configuration
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://ayur-lens-p5glcvecp-pranavgambhire4136s-projects.vercel.app",
     credentials: true, // Allows cookies to be sent
 }));
 
